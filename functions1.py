42ac5355a12eb16e52c90074a1337e1b4f55205d
bulidiriba <bulidiriba@gmail.com>
2018-05-18 08:16:48 +0300

    Update both Network1.py and functions1.py

--- functions1.py
+++ functions1.py
@@ -2,7 +2,9 @@ import numpy as np
 
 
 def softmax(vector):
-    pass
+    exp_vector = np.exp(vector)
+    return exp_vector / np.sum(exp_vector)
+
 
 # v = np.array([1, 10, 0]).reshape((3, 1))
 # print(v)
